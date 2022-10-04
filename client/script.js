d93b96ea55133ed0d5fca61ac2a94df1c83fa102
mohmousad <moh.moussad@gmail.com>
2022-10-04 17:03:09 +0200

    update server url to the heroku

--- client/script.js
+++ client/script.js
@@ -1,7 +1,7 @@
 const searchInput = document.querySelector('.search-input');
 const searchBtn = document.querySelector('.search-btn');
 
-const serverURL = 'http://localhost:3000';
+const serverURL = 'https://nameless-mountain-61465.herokuapp.com/';
 
 searchBtn.addEventListener('click', (e) => {
   e.preventDefault();
