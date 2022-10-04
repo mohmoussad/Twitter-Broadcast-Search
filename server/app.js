57d1e569e2127cbc88d5a960e5f1957be179a13a
mohmousad <moh.moussad@gmail.com>
2022-10-05 01:16:20 +0200

    v.1

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const cors = require('cors');
+require('dotenv').config();
+
 const corsOptions = {
   origin: '*',
-  credentials: true, //access-control-allow-credentials:true
+  credentials: true,
   optionSuccessStatus: 200,
 };
-
-require('dotenv').config();
 
 const app = express();
 app.use(cors(corsOptions));
@@ -18,11 +18,15 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello');
 });
+
 app.get('/search', (req, res) => {
   res.send(JSON.parse('{"msg":"No search input provided"}'));
 });
+
 app.get('/search/:searchInput', async (req, res) => {
-  let searchInput = req.params.searchInput;
+  const searchInput = req.params.searchInput;
+
+  // Guest Token Generation
   const tokenRes = await fetch('https://api.twitter.com/1.1/guest/activate.json', {
     headers: {
       Authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
@@ -31,6 +35,7 @@ app.get('/search/:searchInput', async (req, res) => {
   });
   const { guest_token: guestToken } = await tokenRes.json();
 
+  // Fetching Data
   const fetchAPI = await fetch(`https://twitter.com/i/api/2/search/adaptive.json?q=${searchInput} بث`, {
     headers: {
       authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
@@ -40,6 +45,8 @@ app.get('/search/:searchInput', async (req, res) => {
     method: 'GET',
   });
   const data = await fetchAPI.json();
+
+  // Mapping Data
   const tweets = Object.keys(data.globalObjects.tweets).map((item) => data.globalObjects.tweets[item]);
   const brodcastsTweets = tweets.filter((tweet) => {
     return tweet?.entities?.urls[0]?.expanded_url.includes('broadcasts');
